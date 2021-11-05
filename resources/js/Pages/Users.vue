ea146a13dcc2ae816d3d67d19ea229bb4c7d1680
Jareer <jareer.emedia@gmail.com>
2021-11-05 23:27:45 +0530

    Inertia preserve-scroll added

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,12 +1,25 @@
 <template>
     <h1>Users</h1>
     <Nav/>
+
+    <div style="margin-top:800px">
+        <p>The current time is {{ time }}.</p>
+
+        <Link href="/users" class="text-blue-500 hover:underline" preserve-scroll>
+            Refreash
+        </Link>
+    </div>
 </template>
 
 <script>
-    import Nav from "../Shared/Nav"
+    import Nav from "../Shared/Nav";
+    import { Link } from '@inertiajs/inertia-vue3';
     export default {
-        components:{ Nav },
+        components:{ Nav, Link },
+
+        props:{
+            time:String
+        }
 
     };
 </script>
